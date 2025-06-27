1047857427e08b695f67ee2d64ada6e38ffaae15
Magomed Keligov <kms_91@list.ru>
2025-06-27 20:09:47 +0300

    ФЗ 11 v.1.2

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,8 +4,12 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     List<Film> getAllFilms();
+
     Film getFilmById(int id);
 }
