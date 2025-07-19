1e7a2edb306bad198baad837076fc525b3b53034
Magomed Keligov <kms_91@list.ru>
2025-07-19 16:51:39 +0300

    ФЗ 12

--- src/test/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO mpa_ratings (mpa_id, mpa_name, description)
-VALUES (1, 'G', 'General Audiences'),
-       (2, 'PG', 'Parental Guidance Suggested');
-
-INSERT INTO genres (genre_id, genre_name, description)
-VALUES (1, 'Комедия', 'Веселые фильмы'),
-       (2, 'Драма', 'Серьезные фильмы'),
-       (3, 'Мультфильм', 'Анимационные фильмы');
