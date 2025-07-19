a45a0c7b83ea79c7d3ddddfc4fd0f055c00e02a3
Magomed Keligov <kms_91@list.ru>
2025-07-19 12:13:41 +0300

    ФЗ 12.1

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface GenreStorage {
     List<Genre> getAllGenres();
+
     Genre getGenreById(int id);
+
     List<Genre> getFilmGenres(int filmId);
+
     void addFilmGenre(int filmId, int genreId);
+    
     void removeFilmGenres(int filmId);
 }
