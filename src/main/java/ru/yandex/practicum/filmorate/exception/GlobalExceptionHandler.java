b16ca257accd73b2644b09e4aefe47a4f156394c
Magomed Keligov <kms_91@list.ru>
2025-07-21 00:57:34 +0300

    ФЗ 12.23

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -32,13 +32,13 @@ public class GlobalExceptionHandler {
 //        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 //                .body(Map.of("error", ex.getMessage()));
 //    }
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(NotFoundException exception) {
-        log.warn("Ресурс не найден: {}", exception.getMessage());
-        Map<String, String> body = new HashMap<>();
-        body.put("error", exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
-    }
+//    @ExceptionHandler(NotFoundException.class)
+//    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(NotFoundException exception) {
+//        log.warn("Ресурс не найден: {}", exception.getMessage());
+//        Map<String, String> body = new HashMap<>();
+//        body.put("error", exception.getMessage());
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+//    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleAllExceptions(Exception ex) {
